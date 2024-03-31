f8f2405a56d9c81705acb31aaddf5788e8fd3703
陈卓 <1924492540@qq.com>
2024-03-31 16:08:46 +0800

    更新配置

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/juyingxiaoxiang/",
+  base:"/juyixiaoxiang/",
   plugins: [
     vue(),
     AutoImport({
